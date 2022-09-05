725b961774dd9e7acf2a2f405330e52496151b36
atif298 <atifuddin03@gmail.com>
2022-09-05 08:21:29 +0100

    Morse to English tests created

--- translator.spec.js
+++ translator.spec.js
@@ -1,14 +1,14 @@
 import { translateToMorse } from "./translator";
 
-describe('Testing translateToMorse', () => {
-    it("a should translate to be *-", ()=> {
+describe('Testing translateToMorse for English to Morse', () => {
+    it("a should translate to be .-", ()=> {
       // Arrange
         let string;
       // Act
         string = "a";
         let result = translateToMorse(string);
       // Assert
-      expect(result).toBe("*-");
+      expect(result).toBe(".-");
     });
 
   it("Atif should translate to be .- - .. ..-.", ()=> {
@@ -21,16 +21,11 @@ describe('Testing translateToMorse', () => {
       expect(result).toBe(".- - .. ..-.");
   });
 
-
-  it("Atif should translate to be .- - .. ..-.", ()=> {
-      let string;
-      string = "Atif";
-      let result = translateToMorse(string);
-      expect(result).toBe(".- - .. ..-.");
-  });
-
-  it('space should translate to /', () => {
-    expect(result(" ")).toBe("/");
+  it('a space should translate to /', () => {
+    let string;
+    string = " ";
+    let result = translateToMorse(string);
+    expect(result).toBe("/");
     });
 
 
@@ -64,12 +59,69 @@ describe('Testing translateToMorse', () => {
 
 })
 
-// What should translateToMorse function actually be able to do?
+describe('Testing translateToMorse for Morse toEnglish', () => {
+  it(".- should translate to be A", ()=> {
+    // Arrange
+      let string;
+    // Act
+      string = ".-";
+      let result = translateToMorse(string);
+    // Assert
+    expect(result).toBe("A");
+  });
 
+it(".- - .. ..-. should translate to be ATIF", ()=> {
+    // Arrange
+      let string;
+    // Act
+      string = ".- - .. ..-.";
+      let result = translateToMorse(string);
+    // Assert
+    expect(result).toBe("ATIF");
+});
+
+
+it('/ should translate to a space', () => {
+  let string;
+  string = "/";
+  let result = translateToMorse(string);
+  expect(result).toBe(" ");
+  });
+
+
+it("-.-.-- should translate to be ! ", ()=> {
+  let string;
+  string = "-.-.--";
+  let result = translateToMorse(string);
+  expect(result).toBe("!");
+});
+
+it('.... . .-.. .-.. --- --..-- / -- -.-- / -. .- -- . / .. ... / .- - .. ..-. -.-.-- translate to HELLO, MY NAME IS ATIF! ', () => {
+  let string;
+  string = ".... . .-.. .-.. --- --..-- / -- -.-- / -. .- -- . / .. ... / .- - .. ..-. -.-.--";
+  let result = translateToMorse(string);
+  expect(result).toBe("HELLO, MY NAME IS ATIF!")
+});
+
+it('..--- ...-- ....- translate to 234', () => {
+  let string;
+  string = "..--- ...-- ....-";
+  let result = translateToMorse(string);
+  expect(result).toBe("234")
+});
+
+it("-- -.-- / ..-. .- ...- / -. ..- -- -... . .-. / .. ... --..-- / ..--- ----. ---.. .-.-.- should translate to MY FAV NUMBER IS, 298. ", ()=> {
+  let string;
+  string = "-- -.-- / ..-. .- ...- / -. ..- -- -... . .-. / .. ... --..-- / ..--- ----. ---.. .-.-.-";
+  let result = translateToMorse(string);
+  expect(result).toBe("MY FAV NUMBER IS, 298.");
+});
+
+})
+
+// What should translateToMorse function actually be able to do?
 // - Recieve letters and translate those letters to morse code
 // - Take into cocnsideration spaces between words and translate these accordingly
 // - Numbers
 // - Symbols
-// - Extension - Other languages available other than English
-
 // Should we have a translateToEnglish translate?
